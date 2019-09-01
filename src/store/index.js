e97aece31c925b42bf78c4746e6fe02bd13d2d3d
Dorkside <jdmartin@gmx.fr>
2019-09-01 17:05:27 +0200

    remove axios

--- src/store/index.js
+++ src/store/index.js
@@ -1,20 +1,7 @@
 import Vue from "vue";
 import Vuex from "vuex";
 import VuexORM from "@vuex-orm/core";
-import VuexORMAxios from "@vuex-orm/plugin-axios";
 import database from "@/database";
-
-VuexORM.use(VuexORMAxios, {
-  database,
-  http: {
-    baseUrl: "",
-    url: "/",
-    headers: {
-      Accept: "application/json",
-      "Content-Type": "application/json"
-    }
-  }
-});
 
 Vue.use(Vuex);
 
